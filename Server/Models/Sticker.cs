0538eabf61d55413b47a1857217a2df2c6db7744
mswng <suongdtt1139@ut.edu.vn>
2026-01-16 17:28:26 +0700

    stickers

--- Server/Models/Sticker.cs
+++ Server/Models/Sticker.cs
@@ -1,6 +1,14 @@
-public class Sticker
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ChatServer.Models
 {
-    public string Code { get; set; } = string.Empty; 
-    public string ImageUrl { get; set; } = string.Empty;
-    public string Pack { get; set; } = "default"; 
+    public class Sticker
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Code { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
 }
